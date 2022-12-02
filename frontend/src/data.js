0c20b6e7ef01f252f5b51b5a1382141dbca13801
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2022-12-02 17:42:41 -0600

    added images

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpeg', //679 x 829
+      price: 10,
+      countInStock: 4,
+      brand: 'Nike',
+      rating: 2.5,
+      numReviews: 13,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpeg',
+      price: 40,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit pants',
+      slug: 'adidas-fit-pants',
+      category: 'Pants',
+      image: '/images/p3.jpeg',
+      price: 57,
+      countInStock: 30,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 12,
+      description: 'high quality item',
+    },
+    {
+      name: 'Nike Winter pants',
+      slug: 'nike-slim-pants',
+      category: 'Pants',
+      image: '/images/p4.jpeg',
+      price: 20,
+      countInStock: 22,
+      brand: 'Nike',
+      rating: 4.2,
+      numReviews: 1,
+      description: 'high quality material',
+    },
+  ],
+};
+
+export default data;
